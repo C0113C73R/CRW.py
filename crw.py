282ca899b32c49b9f9b45359077bd51f5444a95f
C0113C73R <44686621+C0113C73R@users.noreply.github.com>
2018-11-01 16:31:01 -0700

    Update crw.py

--- crw.py
+++ crw.py
@@ -1,11 +1,8 @@
-Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:53:40) [MSC v.1500 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import webbrowser
->>> import time
->>> import random
->>> 
->>> while True:
-	sites = random.choice(["google.com", "mastercode.online", "youtube.com", "weather.gov"])
+import webbrowser
+import time
+import random 
+while True:
+    sites = random.choice(["google.com", "mastercode.online", "youtube.com", "weather.gov"])
 	visit = "http://{}".format(sites)
 	webbrowser.open(visit)
 	seconds = random.ranrange(5, 20)
